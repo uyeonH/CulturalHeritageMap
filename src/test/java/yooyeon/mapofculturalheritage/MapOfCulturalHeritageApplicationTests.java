dcf8769b26a3dd8a33513359ec95da072ba5dc73
uyeonH <yooyeon0508@gmail.com>
2022-05-03 05:46:02 +0900

    Initial commit

---
+++ src/test/java/yooyeon/mapofculturalheritage/MapOfCulturalHeritageApplicationTests.java
@@ -0,0 +1,13 @@
+package yooyeon.mapofculturalheritage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapOfCulturalHeritageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
