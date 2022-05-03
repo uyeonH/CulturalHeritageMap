c448ee95ea35d06a453783f8a49aa56fb0c3fcd6
uyeonH <yooyeon0508@gmail.com>
2022-05-03 19:06:58 +0900

    error 공통 처리

--- src/main/java/yooyeon/mapofculturalheritage/domain/user/api/UserController.java
+++ src/main/java/yooyeon/mapofculturalheritage/domain/user/api/UserController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import yooyeon.mapofculturalheritage.domain.user.domain.User;
-import yooyeon.mapofculturalheritage.domain.user.repository.UserRepository;
 import yooyeon.mapofculturalheritage.domain.user.jwt.JwtTokenProvider;
+import yooyeon.mapofculturalheritage.domain.user.repository.UserRepository;
 
 import java.util.Collections;
 import java.util.Map;
